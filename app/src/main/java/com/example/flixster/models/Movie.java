0bf59d3f4e9d206f302b288fe4c027cbc62642fc
Jasper Caballero <jvicaballero@gmail.com>
2021-02-18 02:37:41 -0500

    Completed Required Stories for Flixster Part 2 (Details Activity, Can play trailer for movie selected).

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,17 +3,29 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    int movieId;
     Double voteAverage;
+
+    //empty constructor needed for Parceler library
+    public Movie(){
+
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
@@ -21,6 +33,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         voteAverage = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
